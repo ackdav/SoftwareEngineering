f072a9c6c2a24b2d404fa1163c3f659c94ebf53c
KuersatAydinli <kuersat.aydinli@uzh>
2015-10-28 21:40:15 +0100

    asfasdf

---
+++ WorkspaceSofwareEngineering/MovieDataBase/src/com/google/gwt/sample/MovieDataBase/client/TestClass.java
@@ -0,0 +1,10 @@
+package com.google.gwt.sample.MovieDataBase.client;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallooasdf");
+	}
+
+}
